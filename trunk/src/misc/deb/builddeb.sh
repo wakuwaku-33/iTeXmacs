62ddc1b515264049e821645df84ada1d346c2f04
zohooo <zohooo@cb34d2fa-17a1-548b-9d7e-56c4b6c2eb15>
2010-08-25 03:42:18 +0000

    itexmacs: change install directory to iTeXmacs.

--- trunk/src/misc/deb/builddeb.sh
+++ trunk/src/misc/deb/builddeb.sh
@@ -20,16 +20,16 @@ make -e prefix=$DISTPATH/usr/local install
 mkdir -p $DISTPATH/usr/local/share/applications
 cp -f TeXmacs/misc/mime/itexmacs.desktop $DISTPATH/usr/local/share/applications
 
-rm -rf $DISTPATH/usr/local/share/TeXmacs/.svn
-rm -rf $DISTPATH/usr/local/share/TeXmacs/*/.svn
-rm -rf $DISTPATH/usr/local/share/TeXmacs/*/*/.svn
-rm -rf $DISTPATH/usr/local/share/TeXmacs/*/*/*/.svn
-rm -rf $DISTPATH/usr/local/share/TeXmacs/*/*/*/*/.svn
-rm -rf $DISTPATH/usr/local/share/TeXmacs/*/*/*/*/*/.svn
-rm -rf $DISTPATH/usr/local/share/TeXmacs/*/*/*/*/*/*/.svn
-rm -rf $DISTPATH/usr/local/share/TeXmacs/*/*/*/*/*/*/*/.svn
+rm -rf $DISTPATH/usr/local/share/iTeXmacs/.svn
+rm -rf $DISTPATH/usr/local/share/iTeXmacs/*/.svn
+rm -rf $DISTPATH/usr/local/share/iTeXmacs/*/*/.svn
+rm -rf $DISTPATH/usr/local/share/iTeXmacs/*/*/*/.svn
+rm -rf $DISTPATH/usr/local/share/iTeXmacs/*/*/*/*/.svn
+rm -rf $DISTPATH/usr/local/share/iTeXmacs/*/*/*/*/*/.svn
+rm -rf $DISTPATH/usr/local/share/iTeXmacs/*/*/*/*/*/*/.svn
+rm -rf $DISTPATH/usr/local/share/iTeXmacs/*/*/*/*/*/*/*/.svn
 
 mkdir -p $DISTPATH/DEBIAN
 cp -f misc/deb/DEBIAN/control $DISTPATH/DEBIAN
-dpkg -b $DISTPATH itexmacs.deb
+dpkg -b $DISTPATH iTeXmacs.deb
 
