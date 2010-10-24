c2813705cc585ab9d3af4d4a4547ce053aac99eb
zohooo <zohooo@cb34d2fa-17a1-548b-9d7e-56c4b6c2eb15>
2010-10-24 12:23:54 +0000

    mainmenu: customize simple edit menu in main menu

--- trunk/src/TeXmacs/progs/texmacs/menus/edit-menu.scm
+++ trunk/src/TeXmacs/progs/texmacs/menus/edit-menu.scm
@@ -40,7 +40,7 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; The Edit menu
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
+#!
 (menu-bind edit-menu
   (when (> (undo-possibilities) 0)
     ("Undo" (undo 0)))
@@ -95,6 +95,71 @@
 	  ("Replace" (clipboard-paste "replace"))
 	  ---
 	  ("Other" (interactive clipboard-paste))))
+  ---
+  (-> "Preferences"
+      (link preferences-menu)))
+!#
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; iTeXmacs Edit menu
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(menu-bind edit-menu
+  (when (> (undo-possibilities) 0)
+    ("Undo" (undo 0)))
+  (when (> (redo-possibilities) 0)
+    (if (<= (redo-possibilities) 1)
+    ("Redo" (redo 0)))
+    (if (> (redo-possibilities) 1)
+    (-> "Redo" (link redo-menu))))
+  ---
+  (when (or (selection-active-any?)
+        (and (in-graphics?)
+         (graphics-selection-active?)))
+    ("Copy" (clipboard-copy "primary"))
+    ("Cut" (clipboard-cut "primary")))
+  ("Paste" (clipboard-paste "primary"))
+  (if (detailed-menus?)
+      ("Clear" (clipboard-clear "primary")))
+  ---
+  ("Search" ... (search-start #t))
+  ("Replace" (interactive replace-start-forward))
+  ("Spell" ... (spell-start))
+  ---
+  (when (selection-active-any?)
+    (-> "Copy to"
+      (promise (extern-clipboard-menu-promise "copy-export"))
+      (if (detailed-menus?)
+        ---
+        ("Primary" (clipboard-copy "primary"))
+        ("Secondary" (clipboard-copy "secondary"))
+        ("Ternary" (clipboard-copy "ternary"))
+        ("Search" (clipboard-copy "search"))
+        ("Replace" (clipboard-copy "replace"))
+        ---
+        ("Other" (interactive clipboard-copy))))
+    (-> "Cut to"
+      (promise (extern-clipboard-menu-promise "cut-export"))
+      (if (detailed-menus?)
+        ---
+        ("Primary" (clipboard-cut "primary"))
+        ("Secondary" (clipboard-cut "secondary"))
+        ("Ternary" (clipboard-cut "ternary"))
+        ("Search" (clipboard-cut "search"))
+        ("Replace" (clipboard-cut "replace"))
+        ---
+        ("Other" (interactive clipboard-cut)))))
+  (-> "Paste from"
+    (promise (extern-clipboard-menu-promise "paste-import"))
+      (if (detailed-menus?)
+        ---
+        ("Primary" (clipboard-paste "primary"))
+        ("Secondary" (clipboard-paste "secondary"))
+        ("Ternary" (clipboard-paste "ternary"))
+        ("Search" (clipboard-paste "search"))
+        ("Replace" (clipboard-paste "replace"))
+        ---
+        ("Other" (interactive clipboard-paste))))
   ---
   (-> "Preferences"
       (link preferences-menu)))
